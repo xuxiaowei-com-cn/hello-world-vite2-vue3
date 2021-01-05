bf64b1766ddba21578c4c611448cafdcc1a38e7c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-05 21:58:22 +0800

    el-tag

--- src/views/demo/Ref.vue
+++ src/views/demo/Ref.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <h2>Ref</h2>
-    <div>作者：{{author}}</div>
-    <div>年龄：{{age}}</div>
-    <button @click="nextYear()">下一年</button>
+    <br><br>
+    <el-link type='primary' target='_blank' href='https://v3.vuejs.org/api/refs-api.html'>refs-api</el-link>
+    <br><br>
+    <el-tag>作者：{{author}}</el-tag>
+    <br><br>
+    <el-tag>年龄：{{age}}</el-tag>
+    <br><br>
+    <el-button @click="nextYear()">下一年</el-button>
   </div>
 </template>
 
@@ -12,7 +16,9 @@ import { ref } from 'vue'
 
 const age = ref(26)
 const author = ref('徐晓伟')
+
 function nextYear() {
+  // 只能通过 .value 改变值
   age.value ++;
 }
 
