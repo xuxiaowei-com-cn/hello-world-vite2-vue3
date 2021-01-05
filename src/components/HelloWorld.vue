fef5e74b560b4489877c034d0e490f4d8d493192
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-05 13:32:47 +0800

    element-plus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>{{ msg }}</h1>
-
+  <el-button>默认按钮</el-button>
   <p>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
