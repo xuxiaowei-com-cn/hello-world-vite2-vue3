b33501cf9e2f773ebecb0d7529fd8968b14aae1e
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-06 21:14:16 +0800

    代码抽取

--- vite.config.js
+++ vite.config.js
@@ -8,15 +8,21 @@ import vue from '@vitejs/plugin-vue'
  * @returns {{plugins: [Plugin]}}
  */
 export default ({command, mode}) => {
+    const result = { // 公共部分
+        plugins: [vue()]
+    }
+
     if (command === 'serve') {
         console.log('开发配置')
-        return {
-            plugins: [vue()]
-        }
+        Object.assign(result, { // 开发配置
+
+        });
     } else {
         console.log('生产配置')
-        return {
-            plugins: [vue()]
-        }
+        Object.assign(result, { // 生产配置
+
+        });
     }
+
+    return result
 }
