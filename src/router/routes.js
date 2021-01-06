fbccffe834a0d712e6c009c0a4ed202e306c3618
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-07 00:08:32 +0800

    代码抽取

---
+++ src/router/routes.js
@@ -0,0 +1,23 @@
+import Home from '../views/Home.vue'
+import {refsRef, refsToRef, refsUnref} from './demo'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    refsRef,
+    refsUnref,
+    refsToRef,
+]
+
+export default routes
