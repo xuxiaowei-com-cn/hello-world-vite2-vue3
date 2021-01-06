06d632a0af91245e2289b295233ca85be1135469
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-06 20:59:10 +0800

    条件配置

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import vue from '@vitejs/plugin-vue'
 
 /**
+ * 条件配置
  * @type {import('vite').UserConfig}
+ * @param command 如：serve、build
+ * @param mode 如：development、production
+ * @returns {{plugins: [Plugin]}}
  */
-export default {
-  plugins: [vue()]
+export default ({command, mode}) => {
+    if (command === 'serve') {
+        console.log('开发配置')
+        return {
+            plugins: [vue()]
+        }
+    } else {
+        console.log('生产配置')
+        return {
+            plugins: [vue()]
+        }
+    }
 }
